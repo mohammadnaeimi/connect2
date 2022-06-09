8ab9c5da06aae65e2b87f6441f7fae0ebe0ebb18
mohammadnaeimi <33509546+mohammadnaeimi@users.noreply.github.com>
2022-06-09 10:57:43 +0200

    Update Connect2.py

--- Connect2.py
+++ Connect2.py
@@ -5,7 +5,7 @@ import copy
 
 class Game():
     def __init__(self):
-        self.gamestate = [0, 0, 0, 0, 0, 0, 0, 0] # the state where the games starts
+        self.gamestate = [0, 0, 0, 0, 0, 0, 0, 0] # the state where the games starts, evolves as the game continues
         return
 
     def intial_state(self):
@@ -19,7 +19,7 @@ class Game():
         intital_state[indice[1]] = intital_state[indice[3]] = -1
         return intital_state
 
-    def child_nodes(self, state, m): # returns a list of all possible actions (m = 1 for O(Computer) and m = -1 for X(User))
+    def child_nodes(self, state, m): # returns a list of all possible actions (m = 1 for (Computer) and m = -1 for (User))
         listl = []
         x = copy.deepcopy(state)
         for i in range(len(state)):
@@ -29,7 +29,7 @@ class Game():
                 x = copy.deepcopy(state)
         return listl
 
-    def value(self, n): # returns the value of the terminal nodes as a tuple (if it is a tie the computer wins)
+    def value(self, n): # returns the value of the terminal nodes as a tuple
         O, X = 0, 0
         for i in range(len(n)):
             if i != 0 and i != 7:
@@ -54,26 +54,26 @@ class Game():
             n[x] = m
             return n
 
-    def simulation(self, current_state): # Simulates the entire game from the given node and returns the number of wins on each side and the child nodes (5 moves deep)
+    def simulation(self, current_state, m): # Simulates the entire game from the given node and returns the value of the terminal state on each side and the parent of the terminal
         listm, listn, listt, listh, listp = [], [], [], [], []
         state = []
         count_step = current_state.count(0)
         if count_step == 6:
-            x = self.child_nodes(current_state, 1)
+            x = self.child_nodes(current_state, m)
             for i in x:
-                h = self.child_nodes(i, -1)
+                h = self.child_nodes(i, -1 * m)
                 hovalue, hxvalue = 0, 0
                 for j in h:
-                    m = self.child_nodes(j, 1)
+                    m = self.child_nodes(j, m)
                     movalue, mxvalue = 0, 0
                     for k in m:
-                        n = self.child_nodes(k, -1)
+                        n = self.child_nodes(k, -1 * m)
                         novalue, nxvalue = 0, 0
                         for l in n:
-                            p = self.child_nodes(l, 1)
+                            p = self.child_nodes(l, m)
                             povalue, pxvalue = 0, 0
                             for l in p:
-                                l = [-1 if i == 0 else i for i in l]
+                                l = [-1 * m if i == 0 else i for i in l]
                                 tovalue = self.value(l)[0]
                                 txvalue = self.value(l)[1]
                                 listt.append([tovalue, txvalue])
@@ -92,18 +92,18 @@ class Game():
                 listh.append([hovalue, hxvalue])
             state.append(x)
         if count_step == 5:
-            x = self.child_nodes(current_state, -1)
+            x = self.child_nodes(current_state, -1 * m)
             for i in x:
-                h = self.child_nodes(i, 1)
+                h = self.child_nodes(i, m)
                 hovalue, hxvalue = 0, 0
                 for j in h:
-                    m = self.child_nodes(j, -1)
+                    m = self.child_nodes(j, -1 * m)
                     movalue, mxvalue = 0, 0
                     for k in m:
-                        n = self.child_nodes(k, 1)
+                        n = self.child_nodes(k, m)
                         novalue, nxvalue = 0, 0
                         for l in n:
-                            l = [-1 if i == 0 else i for i in l]
+                            l = [-1 * m if i == 0 else i for i in l]
                             tovalue = self.value(l)[0]
                             txvalue = self.value(l)[1]
                             listt.append([tovalue, txvalue])
@@ -119,15 +119,15 @@ class Game():
                 listh.append([hovalue, hxvalue])
                 state.append(x)
         if count_step == 4:
-            x = self.child_nodes(current_state, 1)
+            x = self.child_nodes(current_state, m)
             for i in x:
-                m = self.child_nodes(i, -1)
+                m = self.child_nodes(i, -1 * m)
                 movalue, mxvalue = 0, 0
                 for j in m:
-                    n = self.child_nodes(j, 1)
+                    n = self.child_nodes(j, m)
                     novalue, nxvalue = 0, 0
                     for k in n:
-                        k = [-1 if i == 0 else i for i in k]
+                        k = [-1 * m if i == 0 else i for i in k]
                         tovalue = self.value(k)[0]
                         txvalue = self.value(k)[1]
                         listt.append([tovalue, txvalue])
@@ -140,12 +140,12 @@ class Game():
                 listh.append([movalue, mxvalue])
             state.append(x)
         if count_step == 3:
-            x = self.child_nodes(current_state, -1)
+            x = self.child_nodes(current_state, -1 * m)
             for i in x:
                 movalue, mxvalue = 0, 0
-                n = self.child_nodes(i, 1)
+                n = self.child_nodes(i, m)
                 for k in n:
-                    k = [-1 if i ==0 else i for i in k]
+                    k = [-1 * m if i ==0 else i for i in k]
                     tovalue = self.value(k)[0]
                     txvalue = self.value(k)[1]
                     movalue = tovalue + movalue
@@ -154,15 +154,15 @@ class Game():
                 listh.append([movalue, mxvalue])
             state.append(x)
         if count_step == 2:
-            x = self.child_nodes(current_state, 1)
+            x = self.child_nodes(current_state, m)
             for k in x:
-                k = [-1 if i == 0 else i for i in k]
+                k = [-1 * m if i == 0 else i for i in k]
                 tovalue = self.value(k)[0]
                 txvalue = self.value(k)[1]
                 listh.append([tovalue, txvalue])
             state.append(x)
         if count_step == 1:
-            current_state = [-1 if i == 0 else i for i in current_state]
+            current_state = [-1 * m if i == 0 else i for i in current_state]
             tovalue = self.value(current_state)[0]
             txvalue = self.value(current_state)[1]
             listh.append([tovalue, txvalue])
@@ -184,31 +184,31 @@ class Game():
         else:
             return 'it is a tie'
 
-    def selection(self, state):
+    def selection(self, state, m): #selects the best node from a given state => strategy: prevents the user from winning
         list_value = []
-        value = self.simulation(state)[0]
-        state = self.simulation(state)[1]
+        value = self.simulation(state, m)[0]
+        state = self.simulation(state, m)[1]
         for i in value:
             list_value.append(i[0])
         return state[0][list_value.index(max(list_value))]
 
-    def play(self): #gets the best first action, gets the user turn and continues the game till the end, you will never win this game
+    def play(self): #selects the best first action, gets the user turn and continues the game till the end, you can never win this game
         self.gamestate[random.randint(1, 5)] = 1
         s1 = self.gamestate
         print(s1)
         self.gamestate = self.get_action(s1, -1)
         s2 = self.gamestate
         print(s2)
-        s3 = self.selection(s2)
+        s3 = self.selection(s2, 1)
         print(s3)
         self.gamestate = self.get_action(s3, -1)
         s4 = self.gamestate
         print(s4)
-        s5 = self.selection(s4)
+        s5 = self.selection(s4, 1)
         print(s5)
         self.gamestate = self.get_action(s5, -1)
         s6 = self.gamestate
-        s7 = self.selection(s6)
+        s7 = self.selection(s6, 1)
         print(s7)
         self.gamestate = self.get_action(s7, -1)
         return self.check_winner(self.gamestate)
